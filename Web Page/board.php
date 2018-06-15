5a65360f084088497ba45f564a8f0516ea080582
김성백 <dumbuk12@naver.com>
2018-06-15 16:24:40 +0900

    Add files via upload
    웹 페이지 관련 HTML 소스 코드

---
+++ Web Page/board.php
@@ -0,0 +1,79 @@
+
+
+<html lang="en">
+<head>
+<title>Table V02</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+<link rel="icon" type="image/png" href="Table/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="Table/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="Table/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="Table/vendor/animate/animate.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="Table/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="Table/vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="Table/css/util.css">
+<link rel="stylesheet" type="text/css" href="Table/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>	
+<div class="limiter">
+<div class="container-table100">
+<div class="wrap-table100">
+<div class="table">
+<div class="row header1">
+<div class ="cell">
+<font size = 24px ><b>Rank</b></font>
+</div>
+<div class ="cell">
+</div>
+</div>
+
+<div class="row header">
+<div class="cell">
+NAME
+</div>
+<div class="cell">
+SCORE
+</div>
+</div><div class="row">
+		<div class="cell" data-title="NAME">test23</div>
+		<div class="cell" data-title="SCORE">2000</div>
+		</div><div class="row">
+		<div class="cell" data-title="NAME">test1</div>
+		<div class="cell" data-title="SCORE">1000</div>
+		</div><div class="row">
+		<div class="cell" data-title="NAME">hosung</div>
+		<div class="cell" data-title="SCORE">420</div>
+		</div><div class="row">
+		<div class="cell" data-title="NAME">test</div>
+		<div class="cell" data-title="SCORE">240</div>
+		</div><div class="row">
+		<div class="cell" data-title="NAME">scw</div>
+		<div class="cell" data-title="SCORE">100</div>
+		</div></div>
+</div>
+</div>
+</div>
+
+
+
+
+<!--===============================================================================================-->	
+<script src="Table/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="Table/vendor/bootstrap/js/popper.js"></script>
+<script src="Table/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="Table/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="Table/js/main.js"></script>
+
+</body>
+</html>
